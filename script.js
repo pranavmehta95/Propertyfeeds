c7d0a65c97e0907c2ab7da7d3b44730903cc5b23
pranavmehta95 <145219665+pranavmehta95@users.noreply.github.com>
2023-11-29 23:09:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add logic to handle form submission, e.g., sending feedback to a server
+    alert("Feedback submitted successfully!");
+  });
